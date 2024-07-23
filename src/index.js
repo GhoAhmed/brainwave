3f84b81e2849341bdf169f919ee75069aae1155d
Ahmed Ghodhbeni <ahmed.godhbeni@gmail.com>
2024-07-24 01:28:48 +0200

    header implementation and design setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
-  </h1>
+  <React.StrictMode>
+    <Router>
+      <>
+        <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+          <Header />
+        </div>
+        <ButtonGradient />
+      </>
+    </Router>
+  </React.StrictMode>
 );
-
