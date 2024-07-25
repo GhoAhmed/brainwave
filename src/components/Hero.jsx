22c5761c3d2e8a5e2a1d13715b5bffb43c7bd3f5
Ahmed Ghodhbeni <ahmed.godhbeni@gmail.com>
2024-07-26 00:59:11 +0200

    Pricing section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
           <h1 className="h1 mb-6">
             Elevate Your &nbsp;Career&nbsp; Potential {` `}
             <span className="inline-block relative">
-              Brainwave{" "}
+              MasterMind{" "}
               <img
                 src={curve}
                 className="absolute top-full left-0 w-full xl:-mt-2"
